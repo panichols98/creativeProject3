3a695484160c40706fe11683c961fcf43ca870b8
panichols98 <panichols98@gmail.com>
2019-10-26 05:17:09 +0000

    final changes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /*global Vue */
 /*global fetch*/
+/*global axios*/
 
 let app = new Vue({
     el: '#app',
@@ -7,23 +8,33 @@ let app = new Vue({
         players: [],
         prefix: '',
         index: 0,
+        viewStats: false,
+        playerData: [],
     },
     methods: {
         async fetchREST() {
-          console.log("In Fetch " + this.prefix);
-          this.index += 1;
-          let id = this.index;
-          let response = await axios("https://www.balldontlie.io/api/v1/players?search=" + this.prefix);
-          if (id != this.index) 
-          {
-              return;
-          }
-            
-          console.log(response);
-          
-          this.players = response.data.data;
-          
-          console.log(this.players);
+            this.viewStats = false;
+            console.log("In Fetch " + this.prefix);
+            this.index += 1;
+            let id = this.index;
+            let response = await axios("https://www.balldontlie.io/api/v1/players?search=" + this.prefix);
+            if (id != this.index) {
+                return;
+            }
+            console.log("response");
+            console.log(response);
+
+            this.players = response.data.data;
+
+            console.log("players");
+            console.log(this.players);
+
         },
-  },
+        async getStats(id) {
+            this.viewStats = true;
+            let response = await axios("https://www.balldontlie.io/api/v1/season_averages?season=2018&player_ids[]=" + id);
+            this.playerData = response.data.data;
+            console.log(this.playerData[0]);
+        },
+    },
 });
